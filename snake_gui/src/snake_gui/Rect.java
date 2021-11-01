00087cdfd11f9c3a5231a705ffde2061cd4cc7de
Administrator <Administrator@205-09>
2021-11-01 15:06:52 +0900

    ...

---
+++ snake_gui/src/snake_gui/Rect.java
@@ -0,0 +1,33 @@
+package snake_gui;
+
+import java.awt.Color;
+public class Rect {
+	
+	private int x , y , width , height;
+	private Color c;
+	
+	public int getX() {
+		return x;
+	}
+	public void setX(int x) {
+		this.x = x;
+	}
+	public int getY() {
+		return y;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	public int getWidth() {
+		return width;
+	}
+	public int getHeight() {
+		return height;
+	}
+	public Color getC() {
+		return c;
+	}
+	public void setC(Color c) {
+		this.c = c;
+	}
+}
