cd31ce1dfef29a0b12727cf5299bfc8e206a5df5
82107 <82107@121.133.253.19>
2021-11-12 18:50:55 +0900

    .

--- gui/src/basic/Ex.java
+++ gui/src/basic/Ex.java
@@ -18,6 +18,8 @@ class ExPanel2 extends MyUtil {
 
 	private Image im = new ImageIcon("image/루피"+1+".png").getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
 	private JLabel image = new JLabel(new ImageIcon(im));
+	private Image im2 = new ImageIcon("image/루피"+1+".png").getImage().getScaledInstance(200, 200, Image.SCALE_SMOOTH);
+	private JLabel image2 = new JLabel(new ImageIcon(im2));
 	
 	public ExPanel2() {
 		setLayout(null);
@@ -28,9 +30,12 @@ class ExPanel2 extends MyUtil {
 		add(this.label); // 컨테이너 위에 add()메소드를 통해 -> 컴포넌트 추가 : 인덱스가 붙음 (추가하는 순서대로)	
 		
 		this.image.setBounds(0,0,500,500);
+		this.image2.setBounds(0,0,600,600);
 	//	this.image.setIcon(new ImageIcon("image/루피.png"));
 		this.image.setVisible(true);
 		add(this.image); // <<<   우선순위 변경 [ 제일먼저 add한것은 변경을 할 수 없음.]
+		add(this.image2);
+		//먼저 추가한것일수록 제일 위로 올라오고 다음에 추가하는건 그 뒤로 깔림.
 //		
 //		//컴포넌트 지우기
 //		remove(image);
